fe746e5a194492290a62e60cd1ede60eeb3e2249
brunopag <paganini.bruno@gmail.com>
2017-10-19 16:57:55 -0300

    Crear marcadores
    Crear marcadores y guardarlos en un array. Seteo de configuracion para que sean draggables.

--- maps/src/app/app.component.ts
+++ maps/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MapsService } from './services/maps.service';
+import { Marcador } from './interfaces/marcador.interfase';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +13,22 @@ export class AppComponent {
   lat: number = -32.9443021;
   lng: number = -60.6728199;
   zoom: number = 16;
+
+  constructor(public _mapsService:MapsService){
+
+  }
+
+  clickMapa( evento ){
+    let nuevoMarcador:Marcador = {
+      lat: evento.coords.lat,
+      lng: evento.coords.lng,
+      draggable: true,
+      titulo: "Nuevo Marcador"
+    }
+
+    this._mapsService.agregarMarcador(nuevoMarcador);
+    
+    console.log(evento);
+  }
 
 }
--- maps/src/app/services/maps.service.ts
+++ maps/src/app/services/maps.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Marcador } from '../interfaces/marcador.interfase';
 
 @Injectable()
 export class MapsService {
 
-  constructor() { }
+  marcadores:Marcador[] = [];
+
+  constructor() {
+    let nuevoMarcador:Marcador = {
+      lat:-32.9443021,
+      lng:-60.6728199,
+      draggable: true,
+      titulo: "Casa"
+    }
+
+    this.marcadores.push(nuevoMarcador);
+   }
+
+   agregarMarcador(marcador:Marcador){
+     this.marcadores.push(marcador);
+   }
 
 }
